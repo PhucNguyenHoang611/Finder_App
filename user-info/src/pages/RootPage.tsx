257a90a2d026d5c64b4027b5ed1ed4a4bcc92f26
Sang <75106540+sang1833@users.noreply.github.com>
2024-05-12 12:05:27 +0700

    width full for rootpage

--- user-info/src/pages/RootPage.tsx
+++ user-info/src/pages/RootPage.tsx
@@ -9,21 +9,23 @@ import WarningAmberOutlinedIcon from "@mui/icons-material/WarningAmberOutlined";
 
 const RootPage = () => {
   return (
-    <main className="flex flex-col lg:flex-row p-[20px]">
+    <main className="flex flex-col lg:flex-row p-[20px] w-full">
       <section className="flex w-full lg:w-4/12 h-full lg:sticky lg:top-[20px]">
         <SideNav />
       </section>
       <section className="w-full lg:w-8/12 h-full mt-[20px] lg:mt-0 lg:ml-[20px] flex flex-col justify-center items-center">
-        <Alert className={cn("rounded-xl bg-red-400 text-white border mb-[20px]")}>
+        <Alert
+          className={cn("rounded-xl bg-red-400 text-white border mb-[20px]")}
+        >
           <WarningAmberOutlinedIcon style={{ color: "white" }} />
           <AlertTitle>Chú ý !</AlertTitle>
 
           <div className="flex gap-1">
+            <AlertDescription>Cảnh báo</AlertDescription>
             <AlertDescription>
-              Cảnh báo
-            </AlertDescription>
-            <AlertDescription>
-              <a href="#" className="underline font-medium">lừa đảo</a>
+              <a href="#" className="underline font-medium">
+                lừa đảo
+              </a>
             </AlertDescription>
           </div>
         </Alert>
